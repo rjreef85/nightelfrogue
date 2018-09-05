79369993f446eff32b76fcfb82d53c72e761079c
rjreef85 <42467786+rjreef85@users.noreply.github.com>
2018-09-05 22:56:33 +0300

    Add files via upload

---
+++ g.py
@@ -0,0 +1,8 @@
+a = input('Введите название блюда или ингредиента')
+if a == 'Recipe Puppy' or a == 'champagne' or a == 'ginger' or a == 'ice' or a == 'vodka' or a =='thumbnail':
+    print('Recipe Puppy')
+if a == 'Potato and Cheese Frittata' or a == 'cheddar cheese' or a == 'eggs' or a == 'olive oil' or a == 'onions' or a == 'potato' or a == 'salt':
+    print('Potato and Cheese Frittata')
+
+if a == 'Succulent Pork Roast' or a == 'brown sugar' or a == 'garlic' or a == 'pork chops' or a == 'water':
+    print('Succelent Pork Roast')
